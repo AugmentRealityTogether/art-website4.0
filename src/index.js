623d3989cb5e7df22f5e5a08c898c089f27f277a
AugmentRealityTogether <art@augmentrealitytogether>
2021-06-09 20:47:41 -0700

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import pARTy from './pARTy';
 
 
 if (window.location.host.split('.')[0] === 'party')
 {
-    console.log("party goes here");
+    //console.log("party goes here");
+    ReactDOM.render(<pARTy />, document.getElementById('root'));
 }
 else {
     ReactDOM.render(<App />, document.getElementById('root'));
