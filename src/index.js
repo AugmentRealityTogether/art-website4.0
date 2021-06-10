736a1f3f2489da643c9416af71794d74a1a3d597
AugmentRealityTogether <art@augmentrealitytogether>
2021-06-09 20:50:36 -0700

    fixed bug with reroute

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import pARTy from './pARTy';
+import PartyC from './pARTy';
 
 
 if (window.location.host.split('.')[0] === 'party')
 {
     //console.log("party goes here");
-    ReactDOM.render(<pARTy />, document.getElementById('root'));
+    ReactDOM.render(<PartyC />, document.getElementById('root'));
 }
 else {
     ReactDOM.render(<App />, document.getElementById('root'));
