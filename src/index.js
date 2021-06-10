56e78cdeeba1d388bfdf0cfda021afc8527c87a8
AugmentRealityTogether <art@augmentrealitytogether>
2021-06-09 20:18:49 -0700

    party console print

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+if (window.location.host.split('.')[0] === 'party')
+{
+    console.log("party goes here");
+}
+else {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
