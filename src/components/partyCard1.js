996cf315ca30483851c7938363805108e726e34b
AugmentRealityTogether <art@augmentrealitytogether>
2021-06-29 13:49:09 -0700

    changed preload status

--- src/components/partyCard1.js
+++ src/components/partyCard1.js
@@ -9,7 +9,7 @@ import invite from "../assets/videos/4thJuly.mp4"
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src={invite} onPlay loop muted /> 
+            <video src={invite} preload={'auto'} autoPlay loop muted /> 
             <br></br>
             <br></br>
             <br></br>
