4cacc69a8e0e8036bb6e421f11845f0ecc09b32e
AugmentRealityTogether <art@augmentrealitytogether>
2021-06-29 11:58:35 -0700

    added new test invite

--- src/components/pages/party.js
+++ src/components/pages/party.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../App.css'
 //import HeroSection from '../HeroSection'
-import Cards from '../partyCard';
+import Cards from '../partyCard1';
 //import Footer from '../Footer';
 
 function Party() {
---
+++ src/components/partyCard1.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
+//import videoIntro from "../assets/videos/video-2.mp4";
+import invite from "../assets/videos/4thJuly.mp4"
+//'../assets/videos/video-2.mp4'
+//import img1 from "../assets/images/ARTNoSpace.png";
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src={invite} autoPlay loop muted /> 
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p> Augment Reality Together</p>
+            <div className='hero-btns'>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    GET STARTED 
+                </Button>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--primary'
+                    buttonSize='btn--large'
+                >
+                    WATCH TRAILER <i className='far
+                    fa-play-circle' />
+                </Button>
+            </div>
+        </div>
+    )
+}
+/*
+<img 
+                    src={img1} 
+                    width = "600"
+                    height = "600"
+                    alt = "Video Intro"
+                    />
+                    */
+export default HeroSection
